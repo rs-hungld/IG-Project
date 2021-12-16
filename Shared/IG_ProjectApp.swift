eb84fb3895ed848cabe5f3afdf9e58b072a9f682
HungLD <hungld@runsystem.net>
2021-12-16 08:59:36 +0700

    Initial Commit

---
+++ Shared/IG_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IG_ProjectApp.swift
+//  Shared
+//
+//  Created by Apple on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct IG_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
